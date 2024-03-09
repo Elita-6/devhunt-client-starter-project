0b2734cdcf077fafa347ef102e734d976b6a3d82
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-09 13:35:06 +0300

    fix deployment error

--- src/app/_common/components/bento_grid.tsx
+++ src/app/_common/components/bento_grid.tsx
@@ -19,54 +19,60 @@ const Skeleton = () => (
 const items = [
     {
         title: "Software engineering and database",
-        description: "Software engineering involves systematic approaches to software development, " +
-            "while databases are structured repositories for efficient data storage and retrieval.",
+        description: "",
         header:  <Image alt="" src="/CyberSecurity.png" width={250} height={50}/>,
         icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
     },
     {
         title: "General computer science",
-        description: "General computer science covers fundamental principles and concepts in computing, such as algorithms, " +
-            "data structures, programming languages, and system architecture.",
+        description: "",
         header: <Image alt="" src="/genielogiciel.jpg" width={250} height={80}/>,
         icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
     },
     {
         title: "Intelligence artificial",
-        description: "Artificial intelligence involves creating computer systems that mimic human intelligence, enabling tasks like learning and problem-solving.",
+        description: "",
         header: <Image alt="" src="/ai.jpg" width={250} height={80}/>,
         icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
     },
     {
         title: "Network and system administrator",
-        description:
-            "Network and system administrators manage and maintain computer networks and systems, ensuring their smooth operation and security.",
+        description: "",
         header: <Image alt="" src="/SystemAdmin.jpg" width={250} height={80}/>,
         icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
     },
     {
         title: "Cybersecurity",
-        description: "Cybersecurity focuses on protecting computer systems, networks, and data from cyber threats and attacks.",
+        description: "",
         header: <Image alt="" src="/CyberSecurity.png" width={250} height={80}/>,
         icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
     },
     {
         title: "Governance and data engineering",
-        description: "Governance of data involves establishing policies and practices for managing data effectively, while data engineering focuses on designing and building systems for collecting, storing, and analyzing data.",
+        description: "",
         header: <Image alt="" src="/CyberSecurity.png" width={250} height={80}/>,
         icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
     },
     {
         title: "Connected objects",
-        description: "Connected objects, also known as IoT devices, are physical devices embedded with sensors, software, and other technologies to connect and exchange data with other devices or systems."
-        ,
+        description: "",
         header: <Image alt="" src="/CyberSecurity.png" width={250} height={70}/>,
         icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
     },
 ];
 export function BentoGridDemo() {
     return (
-        <BentoGrid className=" w-full  px-32 space-y-5 h-fit">
+        <BentoGrid className=" w-full px-32 space-y-5">
+            {items.map((item, i) => (
+                <BentoGridItem
+                    key={i}
+                    title={item.title}
+                    description={item.description}
+                    header={item.header}
+                    icon={item.icon}
+                    // className={i === 3 || i === 6 ? "md:col-span-2 " : ""}
+                />
+            ))}
         </BentoGrid>
     );
 }
