c6b057fde95df26159612180e4edc7fc96d5d9c9
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-09 15:21:43 +0300

    fix deployment error

--- src/app/dashboard/(bolida)/_components/form_bolida.tsx
+++ src/app/dashboard/(bolida)/_components/form_bolida.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {useCreateMessage} from "@/app/dashboard/(bolida)/_hooks/bolida_hooks";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {Send} from "lucide-react";
+import {CircularProgress} from "@mui/material";
 
 interface IFormInput{
     message:string
@@ -12,13 +13,15 @@ const BolidaForm = () => {
         register,
         handleSubmit,
         formState:{errors,isSubmitting},
-        reset
+        reset,
+        resetField
 
     } = useForm<IFormInput>()
     const onSubmit:SubmitHandler<IFormInput> = async (data)=>{
         mutate(data.message)
         console.log("test")
         if (isSuccess){
+            resetField("message")
             console.log(data.message)
             reset()
         }
@@ -30,7 +33,17 @@ const BolidaForm = () => {
                     <input type='text'  placeholder='Type your message here '
                            className='outline-none p-2 input bg-[#E8F4FC] w-[25vw]' {...register("message",{required:true})} />
                     <button type="submit" disabled={isSubmitting}  className="bg-[#0000FF] p-2 rounded-lg">
-                        <Send  className="text-white"/>
+                        {
+                            isLoading ?(
+                                    <div className="flex items-center space-x-2">
+                                        <span className="pl-3 text-white text-xs">Loading...</span>
+                                        <CircularProgress size={15} />
+                                    </div>
+                                ):
+                                <>
+                                    <Send  className="text-white"/>
+                                </>
+                        }
                     </button>
                 </form>
             </div>
